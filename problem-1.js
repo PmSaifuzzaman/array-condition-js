b6b15a0f07c56251da2aa19731933d777baebe5a
Saifuzzaman <pmsaifuzzaman@gmail.com>
2023-07-30 18:52:53 +0600

    problem-1

---
+++ problem-1.js
@@ -0,0 +1,10 @@
+var fruits = ['Apple', 'Banana', 'Orange'];
+
+var fruitsPosition = fruits.indexOf('Banana');
+console.log(fruitsPosition);
+fruits[1] = 'Mango';
+console.log(fruits);
+fruits.pop();
+console.log(fruits);
+fruits.push('Watermelon');
+console.log(fruits);
---
+++ problem-2.js
@@ -0,0 +1,5 @@
+var mine = 85;
+var tom = 66 ;
+var jeny = 95 ;
+var peter = 56;
+var jhon = 40;
